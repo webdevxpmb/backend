6d76cfd73532fcf38f1aecc2f32dee5a3beb3feb
Fachrur Rozi <fachrur97@live.com>
2017-07-24 19:46:09 +0700

    add whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -201,3 +201,5 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
   os.path.join(SITE_ROOT, 'static/'),
 )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
