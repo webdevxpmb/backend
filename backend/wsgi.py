27029e75c059ec96bbf6c741b047374cf672eb9d
Pande Ketut Cahya Nugraha <cahyanugraha12@gmail.com>
2018-07-21 22:53:36 +0700

    use do env

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
