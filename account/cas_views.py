498a99a8fcce5993633db74c59abf269520fa4e0
t-ricky.nursalim <t-ricky.nursalim@traveloka.com>
2018-08-13 00:37:56 +0700

    debug

--- account/cas_views.py
+++ account/cas_views.py
@@ -125,6 +125,7 @@ def login(request, next_page=None, required=False):
                             'name': name, 'npm': npm, 'email': email, 'role': role, 'angkatan': angkatan}
                     return render(request, 'index.html', data)
                 except Exception as e:
+                    logging.debug(user)
                     logging.debug(e)
                     raise
             elif settings.CAS_RETRY_LOGIN or required:
